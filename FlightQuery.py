f87c4312c6c81339163b05bb1807f0ce112c8f6f
fpawelczyk <f.pawelczyk@yahoo.de>
2023-04-10 11:31:38 +0200

    final

--- FlightQuery.py
+++ FlightQuery.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Date: April 9th 2023
+Date: April 10th 2023
 @author: Juan Pablo Brasdefer [225936] (juanbrasdefer) Fabian Pawelczyk [226921] (fpawelczyk)
 """
 from SortedTableMap import *
@@ -8,38 +8,16 @@ from SortedTableMap import *
 class FlightQuery(SortedTableMap):
     '''An application of SortedTableMap, used to query tickets of expected period'''
 
-    # Nested Key class for FlightQuery
-    class Key:
-        __slots__ = "_origin", "_dest", "_date", "_time"
-
-        # Constructor to initialize origin, destination, date, and time
-        def __init__(self, origin, destination, date, time):
-            self._origin = origin
-            self._dest = destination
-            self._date = date
-            self._time = time
-
-        # Less than comparison operator for lexicographic ordering of keys
-        def __lt__(self, other):
-            if self._origin != other._origin:
-                return self._origin < other._origin
-            if self._dest != other._dest:
-                return self._dest < other._dest
-            if self._date != other._date:
-                return self._date < other._date
-            return self._time < other._time
-
     # Query method to find flights within a specified range of keys
     def query(self, k1, k2):
-        # Create Key objects from the provided tuples
-        key1 = self.Key(*k1)
-        key2 = self.Key(*k2)
+        # Find the first flight equal to or greater than k1
+        start = self.find_ge(k1)
 
-        # Iterate through the items in the SortedTableMap and print flights within the specified range
-        for key, value in self.items():
-            if key1 <= key <= key2:
-                print(f"{key._origin} -> {key._dest} @ {key._date} {key._time} : {value}")
-
+        # Iterate through the flights in the SortedTableMap and print flights within the specified range
+        while start and start[0] <= k2:
+            key, value = start
+            print(f"{key[0]} -> {key[1]} @ {key[2]} {key[3]} : {value}")
+            start = self.find_gt(key)
 
 # Example usage
 a = FlightQuery()
@@ -47,7 +25,7 @@ s = [("A", "B", 622, 1200, "No1"), ("A", "B", 622, 1230, "No2"), ("A", "B", 622,
 
 # Populate the FlightQuery object with flight data
 for each in s:
-    key = a.Key(*each[:-1])
+    key = each[:-1]
     value = each[-1]
     a[key] = value
 
